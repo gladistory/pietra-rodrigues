d7858027eae93876efc62ba8dbb26dbc140c9142
Vinicius de Souza Santos <desouza850@gmail.com>
2025-02-09 19:48:45 -0300

    first section pronta e header prontos!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { GuiaComponent } from './components/guia/guia.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: GuiaComponent},
+    {path: 'guia-de-estudos', component: GuiaComponent}
+];
---
+++ src/app/components/guia/guia.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-guia',
+  imports: [],
+  templateUrl: './guia.component.html',
+  styleUrl: './guia.component.css'
+})
+export class GuiaComponent {
+
+}
