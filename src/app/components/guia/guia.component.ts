056714d39a325b05b6fcca0b643ea5b293ecd2f1
Vinicius de Souza Santos <desouza850@gmail.com>
2025-03-20 09:08:03 -0300

    ajustes finais

--- src/app/components/guia/guia.component.ts
+++ src/app/components/guia/guia.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-guia',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './guia.component.css'
 })
 export class GuiaComponent {
+
+  @ViewChild('sessao') sessao!: ElementRef;
+
+  scrollToSection() {
+    this.sessao.nativeElement.scrollIntoView({ behavior: 'smooth' });
+  }
 
 }
